b60858ed17abc33bdff648dad090c192b8789cee
hasferrr <79697518+hasferrr@users.noreply.github.com>
2024-10-28 14:14:56 +0700

    fix: client url

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,7 +12,7 @@ import { connectdb } from './libs/mongodb'
 const app = express()
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: process.env.CLIENT_URL!,
   credentials: true,
 }))
 app.use(express.json())
